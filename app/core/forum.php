a10e442cda51999beb979a343556d7bc20ac5467
481723899123 <012391238712@byom.de>
2017-10-22 14:55:12 +0200

    forum.php

--- app/core/forum.php
+++ app/core/forum.php
@@ -4,7 +4,20 @@
 		public function __construct()
 		{
 			parent::__construct();
+
 			$this->setTitle('Forum');
+			$this->forums = $this->getForums();
+		}
+
+		public function getForums()
+		{
+			$q = $this->db->query('
+				SELECT * FROM forums
+				ORDER BY name ASC
+			');
+
+			$out = $q->fetchAll(PDO::FETCH_OBJ);
+			return $out;
 		}
 	}
 ?>
--- app/view/forum.php
+++ app/view/forum.php
@@ -1,3 +1,20 @@
 		<h2>Foren</h2>
 		<p>Forenübersicht über das aktuelle Geschehen.</p>
+		<table>
+			<tr>
+				<td style="width: 70%;">Forum</td>
+				<td>Letzter Beitrag</td>
+			</tr>
+			<?php foreach($app->forums as $forum): ?>
+			<tr>
+				<td>
+					<a href="<?=$app->path; ?>">Link</a>
+				</td>
+				
+				<td>
+					<a href="<?=$app->path; ?>">Link</a>
+				</td>
+			</tr>
+			<?php endforeach; ?>
+		</table>
 		
