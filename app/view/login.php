9e82fa32e8bb7030b32cacf7087d87a207755068
481723899123 <012391238712@byom.de>
2017-10-22 21:21:17 +0200

    CSS

--- app/view/login.php
+++ app/view/login.php
@@ -2,8 +2,10 @@
 		<p>Bitte gebe Zugansdaten ein um Zugriff auf die Plattform zu erhalten.</p>
 		<?=$app->message; ?>
 		<form method="post">
-			<input type="text" name="name">
-			<input type="password" name="password">
+			<label for="name">Benutzername</label>
+			<input type="text" name="name" id="name" placeholder="BsP. test">
+			<label for="password">Passwort</label>
+			<input type="password" name="password" id="password" placeholder="*****">
 			<input type="submit" value="Login">
 		</form>
 		
